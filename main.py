4489604544a7ec862c556d9aa644083c1106f111
MF-patino <m.fpatino@udc.es>
2025-02-15 18:57:28 +0100

    Added comments and date calculation tweak

--- main.py
+++ main.py
@@ -3,32 +3,41 @@ import requests, pandas
 from pandas import json_normalize
 
 date = 1708452000 #Thu, 20 Feb 2024 18:0:0 GMT
-cnt = 169
+cnt = 169 # number of hourly weather reports we want per call (169 is the maximum)
 url = "https://history.openweathermap.org/data/2.5/history/city?lat=43.36&lon=-8.41&start={}&cnt={}&units=metric&appid=ab105e6ff8fb6bb9aef4b07d7e060f98"
 
+MAX_SIZE = 8000
+
+# accumulators
 size = 0
 data = []
 
-while size < 8000:
+while size < MAX_SIZE:
     size += cnt
 
     with requests.get(url.format(date, cnt), stream=True) as response:
+        # getting the hourly list of weather reports
         jsonData = response.json()['list']
 
         if len(jsonData) != cnt:
             print("Reached end of API feed")
             break
 
+        # mapping function that unpacks the weather field
+        # necessary for conversion into a pandas table later
         def f(e):
             e['weather'] = e['weather'][0]
             return e
 
         data += [f(e) for e in jsonData]
 
-    date += cnt*3600
+        # new date is the most recent date retrieved by the API + an hour (3600 seconds)
+        date = int(jsonData[-1]['dt']) + 3600
 
+# convert json structured data into a table with pandas
 data = json_normalize(data)
 
 print(data.describe())
 
+# save pandas table to disk
 data.to_csv("data.csv")
