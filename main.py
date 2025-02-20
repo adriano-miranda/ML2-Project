ba868a9cabbfe2a5dd05234fa502f1c8a6c0ed67
Blas <baraujo@srec.solutions>
2025-02-20 18:48:59 +0100

    more data

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from river import feature_extraction, linear_model, metrics, preprocessing, stat
 import requests, pandas
 from pandas import json_normalize
 
-date = 1708452000 #Thu, 20 Feb 2024 18:0:0 GMT
+date = 1708617600 #Thu, 22 Feb 2024 17:0:0 GMT
 cnt = 169 # number of hourly weather reports we want per call (169 is the maximum)
 url = "https://history.openweathermap.org/data/2.5/history/city?lat=43.36&lon=-8.41&start={}&cnt={}&units=metric&appid=ab105e6ff8fb6bb9aef4b07d7e060f98"
 
@@ -16,12 +16,13 @@ while size < MAX_SIZE:
     size += cnt
 
     with requests.get(url.format(date, cnt), stream=True) as response:
-        # getting the hourly list of weather reports
-        jsonData = response.json()['list']
-
-        if len(jsonData) != cnt:
+        # checking if the request was not correct
+        if response.status_code != 200:
             print("Reached end of API feed")
             break
+
+        # getting the hourly list of weather reports
+        jsonData = response.json()['list']
 
         # mapping function that unpacks the weather field
         # necessary for conversion into a pandas table later
